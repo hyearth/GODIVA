4d583f34edc6b060b0e5c09c920c8b1475031ef0
camila-Fave <camila@fave.kr>
2025-02-12 22:58:20 +0900

    build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import path, { resolve } from 'path';
 
 export default defineConfig({
-  // base: '/hyearth/', 
-  plugins: [react()],
-})
+	base: './',
+  build: {
+    outDir: './build',
+  },
+});
